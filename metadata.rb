90c2a8e8df3c35cf5c39861a5da8366e146a71db
Simon Detheridge <simon@widgit.com>
2014-09-12 17:47:15 +0100

    Add support for apache 2.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'simon@widgit.com'
 license          'Apache v2.0'
 description      'Sets up an apache vhost site backed by a git repo'
 long_description 'Installs an apache virtual host website using git as a backend. The website is redeployed when files are merged into a specified branch.'
-version          '0.2.0'
+version          '0.2.1'
 
 depends          'apache2'
 depends          'git'
